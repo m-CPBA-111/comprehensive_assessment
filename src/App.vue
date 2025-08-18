2182dc033118357b1329893f7b7cf821e2636e51
LeoTimers <1279493351@qq.com>
2025-08-18 18:07:28 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<!-- 修改后的 App.vue (关键修改) -->
+<template>
+  <v-app>
+    <!-- 使用 RouterView 作为路由出口 -->
+    <RouterView />
+  </v-app>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
